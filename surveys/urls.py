2edd8be9d1416c6f586a0e301015418b78727fe0
oracle4444 <ivanovkir0000@gmail.com>
2021-05-06 17:29:54 +0300

    Developed the surveys page

--- surveys/urls.py
+++ surveys/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.start_page, name='start_page'),
+    path('surveys/', views.surveys, name='surveys'),
 ]
--- surveys/views.py
+++ surveys/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404, get_list_or_404, Http404
+from django.utils import timezone
+
+from .models import Surveys
 
 
 def start_page(request):
     context = {}
     return render(request=request, template_name='surveys/start_page.html', context=context)
+
+
+def surveys(request):
+    try:
+        surveys_list = get_list_or_404(Surveys, finish_date__gte=timezone.now())
+    except Http404:
+        surveys_list = []
+    context = {'surveys': surveys_list}
+    return render(request=request, template_name='surveys/surveys.html', context=context)
