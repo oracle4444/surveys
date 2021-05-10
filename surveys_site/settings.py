7e754750d3021ee7c8e6658013f16198a7d6b7e3
oracle4444 <ivanovkir0000@gmail.com>
2021-05-10 16:47:19 +0300

    Allowed host 0.0.0.0

--- surveys_site/settings.py
+++ surveys_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_qtficu+l$=^em1060k9=%57#-^r))=)xf)zb89w5ngf7)wduz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
