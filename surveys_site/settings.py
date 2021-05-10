6323cf8540afacde62e67c5ae8484d7ca82c7f04
oracle4444 <ivanovkir0000@gmail.com>
2021-05-10 20:07:06 +0300

    Change mode (removed debug)

--- surveys_site/settings.py
+++ surveys_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_qtficu+l$=^em1060k9=%57#-^r))=)xf)zb89w5ngf7)wduz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
